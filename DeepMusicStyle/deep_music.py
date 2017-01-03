4dc9c13afcaea4ea7d68661b7f9dd7339067e0ba
Stephanie Kao <stephanie.kao5@gmail.com>
2017-01-02 23:23:10 -0500

    Music CNN CRNN prediction code

--- DeepMusicStyle/deep_music.py
+++ DeepMusicStyle/deep_music.py
@@ -1,22 +1,29 @@
 import musicCNN.music_tagger_cnn as mtc
-import musicCNN.music_tagger_cnn as mtcr
+import musicCNN.music_tagger_crnn as mtcr
 import musicCNN.audio_processor as ap
 import numpy as np
+import tensorflow as tf
 
-audio_path = ['NeuralStyle/examples/Sparks.wav']
+audio_path = 'music_data/Sparks.wav'
 #,
 #              'NeuralStyle/examples/Heartbeats.wav',
 #              'NeuralStyle/examples/.wav',
 #              'NeuralStyle/Children (Club Radio Edit).wav',
 #              'NeuralStyle/The Wanted - Glad You Came (Instrumental Version).wav'
 
-music_tagger_model = mtc.MusicTaggerCNN(weights='msd', include_top=False)
+music_tagger_model = mtcr.MusicTaggerCRNN(weights='msd', include_top=True)
+#music_tagger_model = mtc.MusicTaggerCNN(weights='msd', include_top=True)
 
-melgrams = np.zeros((0, 1, 96, 1366))
-for path in audio_path:
-    melgrams = np.concatenate(melgrams, ap.compute_melgram(path), axis = 0)
+#melgrams = np.zeros((0, 1, 96, 1366))
+melgrams = ap.compute_melgram(audio_path)
+#for path in audio_path:
+#    melgrams = np.concatenate(melgrams,ap.compute_melgram(path))
 
-features = music_tagger_model.predict(x=melgrams)
+sess = tf.Session()
 
+layer0Output = music_tagger_model.get_layer('bn_0_freq').get_output_at(0).eval(melgrams, sess)
+print(layer0Output)
 
+#features = music_tagger_model.predict(x=melgrams)
 
+#print(features)
